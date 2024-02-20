7a9df1a72f8222fbe5ffd667c5591bddf4925c34
usuf13373301 <usuf13373301@gmail.com>
2024-02-20 18:20:17 +0300

    included glad v2.1, i have to chage it to v4.6 (v 0.0.2)

--- main.cpp
+++ main.cpp
@@ -1,25 +1,36 @@
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
 
 int main() {
-    GLFWwindow* window;
+  GLFWwindow* window;
 
-    if (!glfwInit())
+  if (!glfwInit())
         return -1;
 
-    window = glfwCreateWindow(2560, 1600, "Hello World", NULL, NULL);
-    if (!window) {
-        glfwTerminate();
-        return -1;
-    }
-
-    glfwMakeContextCurrent(window);
-
-    while (!glfwWindowShouldClose(window)) {
-        //glClear(GL_COLOR_BUFFER_BIT);
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
-
+  window = glfwCreateWindow(2560, 1600, "Hello World", NULL, NULL);
+  if (!window) {
     glfwTerminate();
-    return 0;
+      return -1;
+  }
+
+  glfwMakeContextCurrent(window);
+
+  if (!gladLoadGL()) {
+    std::cout << "Can't load glad" << std::endl;
+    return -1;
+  }
+
+  std::cout << "OpenGL" << GLVersion.major << "." << GLVersion.minor << std::endl;
+
+  glClearColor(0, 1, 0, 1);
+
+  while (!glfwWindowShouldClose(window)) {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
+  return 0;
 }
